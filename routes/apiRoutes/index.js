147f0ae60d4a7f120dee38f8edea5c170922e8cb
Paul Han <paul.myung.han@gmail.com>
2020-05-26 15:10:55 -0700

    changed some folder names

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const router = require("express").Router();
-const bookRoutes = require("./books");
-const googleRoutes = require("./google");
+const bookRoutes = require("./bookRoutes");
+const googleRoutes = require("./googleRoutes");
 
 // Book routes
 router.use("/books", bookRoutes);
