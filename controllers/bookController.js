275d0342384ccce0167bf081c8e43f8344d8614b
Paul Han <paul.myung.han@gmail.com>
2020-05-27 13:23:40 -0700

    created the googleController that uses the goodle books api to fetch books based off criteria

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,34 +1,34 @@
 const db = require('../models');
 
 module.exports = {
-    findAll: function(req, res) {
+    findAll: function (req, res) {
         db.Book.find(req.query)
-          .then(dbBook => res.json(dbBook))
-          .catch(err => res.status(422).json(err));
-      },
-    
-    findById: function(req, res) {
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    },
+
+    findById: function (req, res) {
         db.Book.findById(req.params.id)
-          .then(dbBook => res.json(dbBook))
-          .catch(err => res.status(422).json(err));
-      },
-      create: function(req, res) {
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function (req, res) {
         db.Book.create(req.body)
-          .then(dbBook => res.json(dbBook))
-          .catch(err => res.status(422).json(err));
-      },
-      update: function(req, res) {
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function (req, res) {
         db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
-          .then(dbBook => res.json(dbBook))
-          .catch(err => res.status(422).json(err));
-      },
-      remove: function(req, res) {
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function (req, res) {
         db.Book.findById(req.params.id)
-          .then(dbBook => dbBook.remove())
-          .then(dbBook => res.json(dbBook))
-          .catch(err => res.status(422).json(err));
-      }
-    
-    
-    
+            .then(dbBook => dbBook.remove())
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    }
+
+
+
 }
