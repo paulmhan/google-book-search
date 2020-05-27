7bf9debba7b3a9c4a6d1855d96521162cac80fae
Paul Han <paul.myung.han@gmail.com>
2020-05-27 13:19:56 -0700

    create controlller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -12,5 +12,11 @@ module.exports = {
           .then(dbBook => res.json(dbBook))
           .catch(err => res.status(422).json(err));
       },
+      create: function(req, res) {
+        db.Book.create(req.body)
+          .then(dbBook => res.json(dbBook))
+          .catch(err => res.status(422).json(err));
+      },
+    
     
 }
