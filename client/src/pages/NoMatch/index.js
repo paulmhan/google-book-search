e524228a03a3d3cec9bcd0dac5a0cc0bd6d7d040
Paul Han <paul.myung.han@gmail.com>
2020-06-01 00:21:34 -0700

    added page when no books are found

---
+++ client/src/pages/NoMatch/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+
+function NoMatch() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-12">
+          <Jumbotron>
+            <h1 className="text-center">404 Page Not Found</h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default NoMatch;
