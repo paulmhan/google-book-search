7e94dbd711662e08e51155d0ae58000e1368b1e7
Paul Han <paul.myung.han@gmail.com>
2020-06-01 11:48:55 -0700

    changed background color

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import "./style.css";
 
 function App() {
   return (
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -64,7 +64,7 @@ class Home extends Component {
               <h1 className="text-center">
                 <strong>(React) Google Books Search</strong>
               </h1>
-              <h2 className="text-center">Search for and Save Books of Interest.</h2>
+              <h2 className="text-center">Search For Any Book!</h2>
             </Jumbotron>
           </Col>
           <Col size="md-12">
