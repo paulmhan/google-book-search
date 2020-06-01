8a5c6543f84991e2e19d3eba303a54dba2c97136
Paul Han <paul.myung.han@gmail.com>
2020-06-01 00:13:01 -0700

    used grid to render structure and passed book properties into book component

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -32,7 +32,50 @@ class Saved extends Component {
 
   render() {
     return (
-     
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1 className="text-center">
+                <strong>(React) Google Books Search</strong>
+              </h1>
+              <h2 className="text-center">Search for and Save Books of Interest.</h2>
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+            <Card title="Saved Books" icon="download">
+              {this.state.books.length ? (
+                <List>
+                  {this.state.books.map(book => (
+                    <Book
+                      key={book._id}
+                      title={book.title}
+                      subtitle={book.subtitle}
+                      link={book.link}
+                      authors={book.authors.join(", ")}
+                      description={book.description}
+                      image={book.image}
+                      Button={() => (
+                        <button
+                          onClick={() => this.handleBookDelete(book._id)}
+                          className="btn btn-danger ml-2"
+                        >
+                          Delete
+                        </button>
+                      )}
+                    />
+                  ))}
+                </List>
+              ) : (
+                <h2 className="text-center">No Saved Books</h2>
+              )}
+            </Card>
+          </Col>
+        </Row>
+        <Footer />
+      </Container>
     );
   }
 }
