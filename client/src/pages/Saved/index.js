65d1d5378712c080adb0a0f776015604e2851fdd
Paul Han <paul.myung.han@gmail.com>
2020-06-01 00:11:39 -0700

    created saved page with getting saved books function in didMount

---
+++ client/src/pages/Saved/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import Card from "../components/Card";
+import Book from "../components/Book";
+import Footer from "../components/Footer";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import { List } from "../components/List";
+
+class Saved extends Component {
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    this.getSavedBooks();
+  }
+
+  getSavedBooks = () => {
+    API.getSavedBooks()
+      .then(res =>
+        this.setState({
+          books: res.data
+        })
+      )
+      .catch(err => console.log(err));
+  };
+
+
+
+  render() {
+    return (
+     
+    );
+  }
+}
+
+export default Saved;
