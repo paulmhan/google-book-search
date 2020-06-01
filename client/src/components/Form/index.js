836c5d2e89ebff950a451c07f856187dc2d39676
Paul Han <paul.myung.han@gmail.com>
2020-06-01 12:24:17 -0700

    example usage

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -12,7 +12,7 @@ function Form({ q, handleInputChange, handleFormSubmit }) {
           id="Title"
           type="text"
           value={q}
-          placeholder="Ready Player One"
+          placeholder="World War Z"
           name="q"
           onChange={handleInputChange}
           required
