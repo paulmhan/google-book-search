3661f03d87ce2ae02d86268ef0b0ce352f36d6ef
Paul Han <paul.myung.han@gmail.com>
2020-06-01 00:10:00 -0700

    created the home page

--- routes/apiRoutes/bookRoutes/index.js
+++ routes/apiRoutes/bookRoutes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const bookController = require("../../controllers/bookController");
+const bookController = require("../../controllers/bookController.js");
 
 // Matches with "/api/books"
 router.route("/")
